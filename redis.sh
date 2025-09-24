73d8606ef77aafc14442cac57df25fefc32973a6
sivakumar reddy <info@joindevops.com>
2025-09-24 22:46:48 +0530

    github actions

--- redis.sh
+++ redis.sh
@@ -11,6 +11,7 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 mkdir -p $LOGS_FOLDER
+START_TIME=$(date +%s)
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
@@ -46,6 +47,6 @@ systemctl start redis  &>>$LOG_FILE
 VALIDATE $? "Started Redis"
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+TOTAL_TIME=$(( $END_TIME-$START_TIME ))
 
 echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
